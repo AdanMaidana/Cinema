4a8d35ef4f13d45947e8d830a89bbac1051c5f4c
Adán Maidana <adanmaidana31@gmail.com>
2023-12-15 20:29:17 -0300

    vite config -> base modified

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://AdanMaidana.github.io/Cinema",
+  base: "https://adanmaidana.github.io/Cinema/",
 })
