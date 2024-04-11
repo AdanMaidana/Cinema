a4376f544217ecac4a4cdedb5eb8bcd6df096ec9
Adan Maidana <adanmaidana31@gmail.com>
2024-04-11 18:32:10 -0300

    modificado boton en la pagina de welcome

--- src/components/footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-import './footer.css';
-
-export default function Footer() {
-  return (
-    <footer id="footer">
-      <p>Hecho por <a href='https://www.linkedin.com/in/adanmaidana/' target='_blank' rel='noopener noreferrer'>Adán Maidana</a> - 2023</p>
-    </footer>
-  )
-}
